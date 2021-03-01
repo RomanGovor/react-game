6d7f8758d6604ef48374d1522e362064b9130767
Roma Govor <roma-govor2013@ya.ru>
2021-03-02 02:07:07 +0300

    feat: add store

---
+++ minesweeper/src/app/utils/store.js
@@ -0,0 +1,10 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import boardReducer from './reducers/board/reducer';
+import controlsReducer from './reducers/controls/reducer';
+
+const reducers = combineReducers({
+	board: boardReducer,
+	controls: controlsReducer
+});
+
