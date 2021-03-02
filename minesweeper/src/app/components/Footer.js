69ef9706dc037aed4e58de33f1a8997d5866e289
Roma Govor <roma-govor2013@ya.ru>
2021-03-02 12:40:24 +0300

    fix: footer

--- minesweeper/src/app/components/Footer.js
+++ minesweeper/src/app/components/Footer.js
@@ -8,6 +8,7 @@ class Footer extends Component {
                 <div className={"footer wrapper"}>
                     <div className={"footer__author"}><a href={"https://github.com/RomanGovor"} target={"#"}>Author</a></div>
                     <div className={"footer__logo"}><img src={logo} alt={'rs-logo'} /></div>
+                    <div className={"footer__author"}>2021</div>
                     <div className={"footer__courses"}><a href={"https://rs.school/js/"}>JS courses</a></div>
                 </div>
             </footer>
